4ec24ba5b961b75771298a0ed4a34b3d4ffbb642
Michael Marino <michael.marino@TIG365.onmicrosoft.com>
2022-03-09 11:58:02 -0500

    dropdowns for each criteria in configurator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
 import { Component } from '@angular/core';
 
+interface Location {
+  name: string;
+}
+interface FundingSource {
+  name: string;
+}
+interface ChassisType {
+  name: string;
+}
+interface TenantProduct {
+  name: string;
+}
+interface Process {
+  name: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'asset-tagging';
+  locations: Location[] = [
+    { name: 'Anytown High School' },
+    { name: 'Anytown Middle School' },
+    { name: 'Anytown Elementary School' },
+  ];
+  fundingSources: FundingSource[] = [
+    { name: 'Purchase' },
+    { name: 'Donation' },
+  ];
+  chassisTypes: ChassisType[] = [
+    { name: 'Laptop' },
+    { name: 'Chromebook' },
+    { name: 'Desktop' },
+  ];
+  tenantProducts: TenantProduct[] = [
+    { name: 'Asus C204MA Chromebook' },
+    { name: 'Aver X30i Cart' },
+    { name: 'Lenovo 300e Chromebook 2nd Gen' },
+  ];
+  processes: Process[] = [
+    { name: 'Standalone' },
+    { name: 'Carted' },
+    { name: 'Carted on Site' },
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatSelectModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
